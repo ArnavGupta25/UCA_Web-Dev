be3805d606bc3f069373e4714d7f2b9252be3215
ARNAV GUPTA <arnavgupta1551@gmail.com>
2024-11-06 20:39:03 +0530

    deleted class-Project1

--- Class-Project1/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { App as CustomApp } from "./App"
-
-const reactprojectroot = createRoot(document.getElementById("root"));
-reactprojectroot.render(<CustomApp />);
